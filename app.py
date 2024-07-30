2e2bc1696bc60e71c28bc7eefe80d361ea5684ac
Annapreto23 <preto.anna@hotmail.com>
2024-07-30 20:52:19 +0200

    Test

--- app.py
+++ app.py
@@ -1,42 +1,7 @@
-from flask import Flask, request, render_template
-import joblib
-import pandas as pd
+# hello_world.py
 
-app = Flask(__name__)
+def main():
+    print("Hello, World!")
 
-# Load the trained model
-model_filename = 'model/random_forest_model.pkl'
-model = joblib.load(model_filename)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Retrieve form data
-    data = {
-        'Age': float(request.form['Age']),
-        'Gender': int(request.form['Gender']),
-        'EducationLevel': int(request.form['EducationLevel']),
-        'ExperienceYears': float(request.form['ExperienceYears']),
-        'PreviousCompanies': float(request.form['PreviousCompanies']),
-        'DistanceFromCompany': float(request.form['DistanceFromCompany']),
-        'InterviewScore': float(request.form['InterviewScore']),
-        'SkillScore': float(request.form['SkillScore']),
-        'PersonalityScore': float(request.form['PersonalityScore']),
-        'RecruitmentStrategy': int(request.form['RecruitmentStrategy'])
-    }
-
-    # Create DataFrame for new data
-    new_data = pd.DataFrame([data])
-
-    # Make prediction
-    prediction = model.predict(new_data)
-    prediction_result = 'Hired' if prediction[0] == 1 else 'Not Hired'
-
-    # Render the result in the template
-    return render_template('result.html', result=prediction_result)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    main()
