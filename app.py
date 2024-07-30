3eab4a3be4e031f70e3d13fe411adc15428cd587
Annapreto23 <preto.anna@hotmail.com>
2024-07-30 20:54:50 +0200

    Test

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-# hello_world.py
+from flask import Flask
 
-def main():
-    print("Hello, World!")
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
